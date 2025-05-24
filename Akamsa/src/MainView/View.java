51a8e89f9d3d88910c88ae1ba8de36140ff7a92c
ValentinoAbinata <valentabinata@gmail.com>
2025-05-24 23:08:01 +0700

    Initiate

---
+++ Akamsa/src/MainView/View.java
@@ -0,0 +1,24 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package MainView;
+
+/**
+ *
+ * @author ASUS
+ */
+public class View {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        SignIn v = new SignIn();
+        v.setVisible(true);
+        v.setLocationRelativeTo(null);
+
+    }
+    
+}
